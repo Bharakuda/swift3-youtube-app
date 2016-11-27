75d59c609cac8d44d9a3694faba628a138e446f2
Bharakuda <alisa.kopric@gmail.com>
2016-11-27 18:09:11 +0100

    Initial Commit

---
+++ YouTubeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YouTubeApp
+//
+//  Created by Alisa Koprić on 27/11/16.
+//  Copyright © 2016 Alisa Koprić. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
